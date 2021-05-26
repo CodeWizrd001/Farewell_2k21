1e4104364391c240d5a1a316b3a06fe79fba4120
Wizard <codewizard001@gmail.com>
2021-05-26 07:34:10 -0400

    Added Template Lists Page 	Added Button To Switch Between Pages

---
+++ js/common.js
@@ -0,0 +1,62 @@
+var list = [
+    {
+        id:0,
+        title:'Kick Vimal' ,
+        text:'Vote To Kick Vimal Initiated',
+        sign:'Anonymous' ,
+    },
+    {
+        id:1,
+        title:'High' ,
+        text:'Syam is high',
+        sign:'Anonymous' ,
+    },
+    {
+        id:2,
+        title:'Vibe' ,
+        text:'Amal ne kondu thottu',
+        sign:'Anonymous' ,
+    },
+    {
+        id:3,
+        title:'Souj' ,
+        text:'Soujanya serves Mud Coffee',
+        sign:'Anonymous' ,
+    },
+    {
+        id:4,
+        title:'Me' ,
+        text:'How Cool Am I ?',
+        sign:'Anonymous' ,
+    },
+    {
+        id:5,
+        title:'Question' ,
+        text:'How you doing ?',
+        sign:'Ajay' ,
+    },
+    {
+        id:6,
+        title:'Topper' ,
+        text:'Siddhu Is A Body Builder ?',
+        sign:'Anonymous' ,
+    },
+    {
+        id:7,
+        title:'Sova' ,
+        text:'Vishnu is Sova main . Aano ?',
+        sign:'Anonymous' ,
+    },
+    {
+        id:8,
+        title:'Toxic' ,
+        text:'Viper Kollaam',
+        sign:'Anonymous' ,
+    },
+    {
+        id:9,
+        title:'Joke' ,
+        text:'There is a ship in relationship. Is that why Couple sounds like kappal ?',
+        sign:'Anonymous' ,
+    },
+];
